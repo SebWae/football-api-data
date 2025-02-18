63b6e9f61a50c80aabd21e96f36f9fd1b751467f
swae <swae@itu.dk>
2025-02-18 21:25:35 +0100

    dictionaries to keep track of api data when loaded

---
+++ data_dicts.py
@@ -0,0 +1,37 @@
+# dictionaries to store data from api calls before uploading to google big query
+data_dicts = {
+    "fixture_data": {
+        "fixture_id": [],
+        "referee": [],
+        "timezone": [],
+        "date": [],
+        "kick_off": [],
+        "venue_id": [],
+        "venue_name": [],
+        "city": [],
+        "elapsed_time": [],
+        "extra_time": [],
+        "league_id": [],
+        "league_name": [],
+        "season": [],
+        "stage": [],
+        "home_team_id": [],
+        "home_team_name": [],
+        "away_team_id": [],
+        "away_team_name": [],
+        "home_ft": [],
+        "away_ft": [],
+        "home_ht": [],
+        "away_ht": [],
+        "home_et": [],
+        "away_et": [],
+        "home_pen": [],
+        "away_pen": []
+        },
+
+    "team_data": {
+        "team_id": [],
+        "team_name": [],
+        "team_logo": []
+        }
+}
