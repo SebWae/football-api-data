9d5ae6b6541b58bfe7ae752b1abac80292be9053
SebWae <s254120@dtu.dk>
2026-02-09 21:28:47 +0100

    moved to scripts folder

---
+++ scripts/register_fixtures.py
@@ -0,0 +1,18 @@
+import sys
+from pathlib import Path
+
+from tqdm import tqdm
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from api_calls import register_fixtures
+from utils import generate_date_list
+
+
+# registering fixtures in date interval
+start_date = "2026-01-20"
+end_date = "2026-02-08"
+dates = generate_date_list(start_date, end_date)
+
+for date in tqdm(dates):
+    register_fixtures(date)
